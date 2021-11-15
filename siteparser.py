9eb3a3da704f6932e1976514c30619d8d6bcaa9c
Fedor Stolpovskiy <54177005+FedorStolpovskiy@users.noreply.github.com>
2021-11-16 00:03:11 +0300

    Update siteparser.py

--- siteparser.py
+++ siteparser.py
@@ -3,7 +3,12 @@ from selenium import webdriver
 
 
 def main(url):
-    driver = webdriver.Chrome()
+    chrome_options = webdriver.ChromeOptions()
+    chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
+    chrome_options.add_argument("--headless")
+    chrome_options.add_argument("--disable-dev-shm-usage")
+    chrome_options.add_argument("--no-sandbox")
+    driver = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), chrome_options=chrome_options)
     driver.get(url)
     btn = driver.find_element_by_class_name('covid-table-view__expand')
     btn.click()
